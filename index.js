f65385686178b32b76162a44a2863047501756c1
Alsymon <alsymon1038@gmail.com>
2023-12-08 22:32:48 +0800

    fixed

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-let productList = [];
+let productList = [
+        { id: 'product1', name: 'Product 1', description: 'Description 1', price: '100', image: 'product1.jpg' },
+        { id: 'product2', name: 'Product 2', description: 'Description 2', price: '200', image: 'product2.jpg' },
+        { id: 'product3', name: 'Product 3', description: 'Description 3', price: '300', image: 'product3.jpg' }
+    ];
+
 
 function registerUser() {
     let firstname = document.querySelector("#firstname");
@@ -73,52 +78,58 @@ if (btnAddProduct) {
     });
 }
 
-deleteButtons = document.querySelectorAll(".btn-danger");
+let deleteButtons = document.querySelectorAll(".btn-danger");
 
-deleteButtons.forEach(function (button) {
-    button.addEventListener("click", function () {
-        let productCard = button.closest(".row1, .row2, .row3");
+    deleteButtons.forEach(function (button) {
+        button.addEventListener("click", function () {
 
-        if (productCard) {
-            productCard.remove();
+            let productCard = button.closest(".row1, .row2, .row3");
 
-            productId = productCard.querySelector('.btn-outline-success').getAttribute('data-product-id');
-            updatedProductList = productList.filter(product => product.id !== productId);
-            productList = updatedProductList;
+            if (productCard) {
+                productCard.remove();
 
-            localStorage.setItem("productList", JSON.stringify(productList));
-        }
+            }
+        });
     });
-});
+
 
 function populateProductDropdown() {
-     updateProductDropdown = document.getElementById('updateProductDropdown');
+    let updateProductDropdown = document.getElementById('updateProductDropdown');
 
     productList.forEach(product => {
-        option = document.createElement('option');
+        let option = document.createElement('option');
         option.value = product.id;
-        option.textContent = product.productName;
+        option.textContent = product.name; 
         updateProductDropdown.appendChild(option);
     });
 }
 
 function handleUpdateProduct() {
-      selectedProductId = document.getElementById('updateProductDropdown').value;
-      updatedProductName = document.getElementById('updatedProductName').value;
-      updatedProductDescription = document.getElementById('updatedProductDescription').value;
-      updatedProductPrice = document.getElementById('updatedProductPrice').value;
+    let selectedProductId = document.getElementById('updateProductDropdown').value;
+    let updatedProductName = document.getElementById('updatedProductName');
+    let updatedProductDescription = document.getElementById('updatedProductDescription');
+    let updatedProductPrice = document.getElementById('updatedProductPrice');
+    let updatedFileInput = document.getElementById('updatedFileInput');
+    let updatedProductImage = updatedFileInput.files.length > 0 ? updatedFileInput.files[0].name : 'default.jpg';
 
-      updatedProductIndex = productList.findIndex(product => product.id === selectedProductId);
+    let updatedProductIndex = productList.findIndex(product => product.id === selectedProductId);
 
     if (updatedProductIndex !== -1) {
-        productList[updatedProductIndex].productName = updatedProductName;
-        productList[updatedProductIndex].description = updatedProductDescription;
-        productList[updatedProductIndex].price = updatedProductPrice;
+        productList[updatedProductIndex].name = updatedProductName.value;
+        productList[updatedProductIndex].description = updatedProductDescription.value;
+        productList[updatedProductIndex].price = updatedProductPrice.value;
+        productList[updatedProductIndex].image = updatedProductImage;
 
+        updatedProductName.value = "";
+        updatedProductDescription.value = "";
+        updatedProductPrice.value = "";
+        updatedFileInput.value = ""; // Clear the file input
+        console.log('Product updated:', productList[updatedProductIndex]);
         localStorage.setItem("productList", JSON.stringify(productList));
     }
 }
 
-populateProductDropdown();
-
+document.getElementById('btnAddProduct').addEventListener('click', btnAddProduct);
 document.getElementById('btnUpdateProduct').addEventListener('click', handleUpdateProduct);
+
+populateProductDropdown();
